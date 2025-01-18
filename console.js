1c613c62db077cc36ed6886dbbc8ed469ff08033
dinesh-thurpati <dinesheshan.852@gmail.com>
2025-01-18 13:25:25 +0530

    Add files via upload

---
+++ console.js
@@ -0,0 +1,12 @@
+console.log("hii i am javascript")
+
+let a = 20
+let b = 35
+console.log(a%b)
+console.log(a-b)
+console.log(a+b)
+console.log(a*b)
+console.log(a/b)
+
+alert("please text here: ")
+document.write("hello i am external js")
